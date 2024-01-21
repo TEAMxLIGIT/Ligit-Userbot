beff485df65bbb92bb402617a047bc4ed32305c1
TEAMxLIGIT <131442655+TEAMxLIGIT@users.noreply.github.com>
2024-01-21 12:27:03 +0530

    Update inline.py

--- Zaid/modules/bot/inline.py
+++ Zaid/modules/bot/inline.py
@@ -64,7 +64,7 @@ async def alive_function(message: Message, answers):
         InlineQueryResultArticle(
             title="Alive",
             description="Check Bot's Stats",
-            thumb_url="https://te.legra.ph/file/073d2e0a97e9554e55ef9.jpg",
+            thumb_url="https://te.legra.ph/file/d735867c2930509c1283b.jpg",
             input_message_content=InputTextMessageContent(
                 msg, parse_mode=ParseMode.HTML, disable_web_page_preview=True
             ),
@@ -82,7 +82,7 @@ async def help_function(answers):
         InlineQueryResultArticle(
             title="Help Article!",
             description="Check Command List & Help",
-            thumb_url="https://te.legra.ph/file/073d2e0a97e9554e55ef9.jpg",
+            thumb_url="https://te.legra.ph/file/d735867c2930509c1283b.jpg",
             input_message_content=InputTextMessageContent(
                 Data.text_help_menu.format(len(CMD_HELP))
             ),
